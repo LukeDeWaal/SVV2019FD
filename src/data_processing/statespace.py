9876811cd9887d9e9d0616e99a0204c540ba46b8
Matheus Coelho Silva <msilva95@ufl.edu>
2019-03-06 11:45:06 +0100

    Finally fixed SS response

--- src/data_processing/statespace.py
+++ src/data_processing/statespace.py
@@ -29,20 +29,20 @@ Cmde = -1.1642
 
 #C1*x' + C2*x + C3*u = 0
 
-C1 = np.array([[-2.0*muc*c, 0.0, 0.0, 0.0], 
+C1 = np.array([[-2.0*muc*c/V0, 0.0, 0.0, 0.0], 
                [0.0, (CZadot - 2.0*muc)*c/V0, 0.0, 0.0], 
                [0.0, 0.0, -c/V0, 0.0],
-               [0.0, Cmadot, 0.0, -2.0*muc*KY2]])
+               [0.0, Cmadot*c/V0, 0.0, -2.0*muc*KY2*c/V0]])
 
-C2 = np.array([[V0*CXu, V0*CXa, V0*CZ0, V0*CXq], 
-               [CZu, CZa, -CX0, (CZq + 2.0*muc/V0)], 
+C2 = np.array([[CXu, CXa, CZ0, CXq], 
+               [CZu, CZa, -CX0, (CZq + 2.0*muc)], 
                [0.0, 0.0, 0.0, 1.0], 
-               [Cmu*V0/c, Cma*V0/c, 0.0, Cmq*V0/c] ])
+               [Cmu, Cma, 0.0, Cmq] ])
 
-C3 = np.array([[CXde*V0],
+C3 = np.array([[CXde],
                [CZde],
                [0.0],
-               [Cmde*V0/c]])
+               [Cmde]])
 
 #x' = A*x + B*u
 #y  = C*x + D*u
@@ -65,57 +65,18 @@ t = np.linspace(0.0, 300.0, num = 200)
 
 t, y = control.step_response(system, t, x0)
 
-# plt.figure(1)
-# plt.plot(t, y[0,:])
-# plt.xlabel("Time [s]")
-# plt.ylabel("u (disturbance in velocity) [m/s]")
-
-
 fig = plt.figure()
 ax1 = fig.add_subplot(221)
 ax1.plot(t, y[0, :])
 # ax1.xlabel("Time [s]")
 # ax1.ylabel("u (disturbance in velocity) [m/s]")
 
-# plt.figure(3)
-# plt.plot(t, y[2, :])
-# plt.xlabel("Time [s]")
-# plt.ylabel("theta (pitch angle)) [rad]")
-
-<<<<<<< HEAD
-plt.figure(4)
-plt.plot(t, y[3, :])
-plt.xlabel("Time [s]")
-plt.ylabel("q (pitching rate) [rad/s]")
-=======
 ax2 = fig.add_subplot(222)
 ax2.plot(t, y[1, :])
 ax3 = fig.add_subplot(223)
 ax3.plot(t, y[3, :])
 ax4 = fig.add_subplot(224)
 ax4.plot(t, y[3, :])
-
-
-# plt.figure(1)
-# plt.plot(t, y[0, :])
-# plt.xlabel("Time [s]")
-# plt.ylabel("u (disturbance in velocity) [m/s]")
-#
-# plt.figure(2)
-# plt.plot(t, y[1, :])
-# plt.xlabel("Time [s]")
-# plt.ylabel("alpha (AoA) [rad]")
-#
-# plt.figure(3)
-# plt.plot(t, y[2, :])
-# plt.xlabel("Time [s]")
-# plt.ylabel("theta ()) [rad]")
-#
-# plt.figure(4)
-# plt.plot(t, y[3, :])
-# plt.xlabel("Time [s]")
-# plt.ylabel("q (pitching rate)) [rad/s]")
->>>>>>> 01d2c6d1844d896f65905491610a7f36f831df47
 
 plt.show()
 
