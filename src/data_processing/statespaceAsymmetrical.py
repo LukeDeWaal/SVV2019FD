cd95eec4321202f6a54021ce7b75bcca466db150
Matheus Coelho Silva <msilva95@ufl.edu>
2019-03-06 12:27:21 +0100

    Added SS representation of asymmetrical EoM for abritrary pulse input

---
+++ src/data_processing/statespaceAsymmetrical.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 15:40:15 2019
+
+@author: Matheus
+"""
+import control
+import control.matlab
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+
+#Import reference values:
+
+dirname = os.path.dirname(os.path.realpath(__file__))
+Cit_parStr = os.path.join(dirname, r'..\\..\\data')
+Cit_parStr = os.path.abspath(os.path.realpath(Cit_parStr))
+
+sys.path.append(Cit_parStr)
+
+from Cit_par import *
+
+#These reference values are missing from Cit_par:
+
+Cma = -0.5626
+Cmde = -1.1642
+
+#State-space representation of asymmetric EOM:
+
+#C1*x' + C2*x + C3*u = 0
+
+C1 = np.array([[(CYbdot - 2.0*mub)*b/V0, 0.0, 0.0, 0.0], 
+               [0.0, -0.5*b/V0, 0.0, 0.0], 
+               [0.0, 0.0, -4.0*mub*KX2*b/V0, 4.0*mub*KXZ*b/V0],
+               [Cnbdot*b/V0, 0.0, 4.0*mub*KXZ*b/V0, -4.0*mub*KZ2*b/V0]])
+
+C2 = np.array([[CYb, CL, CYp, (CYr - 4.0*mub)], 
+               [0.0, 0.0, 1.0, 0.0], 
+               [Clb, 0.0, Clp, Clr], 
+               [Cnb, 0.0, Cnp, Cnr] ])
+
+C3 = np.array([[CYda, CYdr],
+               [0.0, 0.0],
+               [Clda, Cldr],
+               [Cnda, Cndr]])
+
+#x' = A*x + B*u
+#y  = C*x + D*u
+
+# now u = [da]
+#         [dr]
+
+A = -np.matmul(np.linalg.inv(C1), C2)
+B = -np.matmul(np.linalg.inv(C1), C3)
+C = np.identity(4) #y = x, meaning we output the state
+D = np.array([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0]])
+
+#Make control.ss state-space
+
+system = control.ss(A, B, C, D)
+
+x0 = np.array([[0.0], 
+               [0.0], 
+               [0.0], 
+               [0.0]])
+
+t = np.linspace(0.0, 300.0, num = 200)
+
+#Input:
+u = np.zeros((2, t.shape[0]))
+
+#length of pulse
+tpulse = 4.0
+i = 0
+while (t[i] < tpulse):
+    #u[0, i] = 1.0 #in this case the first input (da) gets a pulse
+    u[1, i] = 1.0 #in this case the second input (dr) gets a pulse
+    i += 1
+    
+t, y, x = control.forced_response(system, t, u, x0, transpose=False)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(221)
+ax1.plot(t, y[0, :])
+# ax1.xlabel("Time [s]")
+# ax1.ylabel("u (disturbance in velocity) [m/s]")
+
+ax2 = fig.add_subplot(222)
+ax2.plot(t, y[1, :])
+ax3 = fig.add_subplot(223)
+ax3.plot(t, y[3, :])
+ax4 = fig.add_subplot(224)
+ax4.plot(t, y[3, :])
+
+plt.show()
+
+
+
